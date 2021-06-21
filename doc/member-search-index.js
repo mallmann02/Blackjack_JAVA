29323fd8831ffbfe90dbd65043f39ab4d339b22b
mallmann02 <leomallmann020302@gmail.com>
2021-06-21 16:10:41 -0300

    Generate javadoc

---
+++ doc/member-search-index.js
@@ -0,0 +1,1 @@
+memberSearchIndex = [{"p":"<Unnamed>","c":"MainApp","l":"main(String[])","u":"main(java.lang.String[])"},{"p":"<Unnamed>","c":"MainApp","l":"MainApp()","u":"%3Cinit%3E()"}];updateSearchResults();
