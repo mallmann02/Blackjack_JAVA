ae48af340ae3141798332248922b655fcfc9bc2c
mallmann02 <leomallmann020302@gmail.com>
2021-06-22 13:16:15 -0300

    Generate new documentation

--- doc/member-search-index.js
+++
@@ -1,1 +0,0 @@
-memberSearchIndex = [{"p":"<Unnamed>","c":"MainApp","l":"main(String[])","u":"main(java.lang.String[])"},{"p":"<Unnamed>","c":"MainApp","l":"MainApp()","u":"%3Cinit%3E()"}];updateSearchResults();
